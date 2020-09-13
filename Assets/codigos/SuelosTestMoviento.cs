f16b20b2d988e281d3d0d237c861699e40f55fa6
Oscarleif <oscar.leif@gmail.com>
2020-09-13 18:52:24 -0500

    Problema solucionado.

--- Assets/codigos/SuelosTestMoviento.cs
+++ Assets/codigos/SuelosTestMoviento.cs
@@ -7,27 +7,38 @@ public class SuelosTestMoviento : MonoBehaviour
 {
     public float velocidadJuego = 1;
     public Transform emphtyReposicionar;
-    
+    private float limitZ;
+
+    private void Start()
+    {
+        limitZ = transform.position.z;
+    }
+
     private void Update()
-    {   
-        transform.Translate(0, 0, Time.deltaTime * -velocidadJuego);
+    {
 
 
-        if(this.transform.position.z < -40)
+        if (this.transform.position.z <= limitZ - 40f)
         {
-            float posicionEmphty =  emphtyReposicionar.position.z + 20;
+            //Debug.Break();
+            //float posicionEmphty = emphtyReposicionar.position.z + 20;
             print("el suelo tendría que reposicionarse");
             //this.transform.Translate(new Vector3(this.transform.position.x,-30,this.transform.position.z),Space.World);
-          
+
             this.transform.position = new Vector3(
                 this.transform.position.x,
                 this.transform.position.y,
-                posicionEmphty
+                this.transform.position.z + 40
                 );
-            
-            print("la posicion del empty es: " + posicionEmphty);
-            print("la posicion de la plataforma es : " + this.transform.position.z);
-            print("----------------------------------------------------------------");     
+
+
+            //print("la posicion del empty es: " + posicionEmphty);
+            //print("la posicion de la plataforma es : " + this.transform.position.z);
+            //print("----------------------------------------------------------------");
+        }
+        else
+        {
+            transform.Translate(0, 0, Time.deltaTime * -velocidadJuego);
         }
     }
 
